ace448e777be38b70522936c4c3bc5b1a0c31e0a
AntMendez <amendezr@fi.uba.ar>
2024-08-12 10:04:56 -0300

    subiendo css y js

---
+++ script.js
@@ -0,0 +1,44 @@
+window.onload = init;
+function init(){
+    document.getElementById("hms").innerHTML="00:00:00";
+    h = 0;
+    m = 0;
+    s = 0;
+    document.querySelector(".start").addEventListener("click",cronometrar);
+    document.querySelector(".stop").addEventListener("click",parar);
+    document.querySelector(".reset").addEventListener("click",reiniciar);
+}   
+
+function cronometrar(){
+    escribir();
+    start_interval=setInterval(escribir,1000);
+    document.querySelector(".start").removeEventListener("click",cronometrar);
+
+}
+
+function escribir(){
+    s++;
+    if (s>59){m++;s=0}
+    if (m>59){h++;s=0}
+    if (h>24){alert('Pasaron mas de 24hs'),reiniciar()}
+
+    var sAux, mAux, hAux
+    if (s<10){sAux='0'+s;}else{sAux=s}
+    if (m<10){mAux='0'+m;}else{mAux=m}
+    if (h<10){hAux='0'+h;}else{hAux=h}
+    document.getElementById('hms').innerHTML= hAux + ':' + mAux + ':' + sAux;
+}
+
+function parar(){
+    clearInterval(start_interval)
+    document.querySelector(".start").addEventListener("click",cronometrar);
+
+}
+
+function reiniciar(){
+    clearInterval(start_interval)
+    document.getElementById("hms").innerHTML="00:00:00";
+    h = 0;
+    m = 0;
+    s = 0;
+}
